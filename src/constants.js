56beb7d531f661a22730c96914881fdc73da866c
nangelos <nicksangelos@gmail.com>
2018-11-20 15:27:10 -0600

    [constants] more text changes

--- src/constants.js
+++ src/constants.js
@@ -1,17 +1,17 @@
-export const team = [
-  {
-    name: 'Robert Wheatfall',
-    bio:
-      "Robert has a background in this thing and that thing, and has a strong interest in creating the future of retail. He graduated with a B.A. from Morehouse College and an MBA from Harvard Business School. Robert's responsibilities include project one, project two, and project three.",
-    photo: 'location of photo',
-  },
-  {
-    name: 'Nick Angelos',
-    bio:
-      "Nick has a background in software development and corporate strategy, and is interested in something. He graduated with a B.S. in Economics from Brigham Young University. Nick's responsibilities include software development, operational streamlining, due diligence on acquisition targets.",
-    photo: 'location of photo',
-  },
-];
+// export const team = [
+//   {
+//     name: 'Robert Wheatfall',
+//     bio:
+//       "Robert has a background in this thing and that thing, and has a strong interest in creating the future of retail. He graduated with a B.A. from Morehouse College and an MBA from Harvard Business School. Robert's responsibilities include project one, project two, and project three.",
+//     photo: 'location of photo',
+//   },
+//   {
+//     name: 'Nick Angelos',
+//     bio:
+//       "Nick has a background in software development and corporate strategy, and is interested in something. He graduated with a B.S. in Economics from Brigham Young University. Nick's responsibilities include software development, operational streamlining, due diligence on acquisition targets.",
+//     photo: 'location of photo',
+//   },
+// ];
 
 export const aboutUs = [
   'Eddie was started because we wanted to create a better customer experience in the home improvement industry. We believe that the home improvement and home repair industries should have access to all the convenience and technology that other industries use. The home improvement industry is a highly fragmented $400B industry, but you can count on Eddie for all your home repairs.',
@@ -23,4 +23,3 @@ export const whatWeDo =
 
 export const whereWeAre =
   'We currently service neighborhoods on the North and West sides of Chicago, with plans to add additional neighborhoods in the near future.';
-// 'Being strategically located in Chicago ensures we never forget our roots, while maintaining access to all that our country has to offer.';
