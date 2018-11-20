f5b80e4300598111cc708fe74f1baec413d52468
nangelos <nicksangelos@gmail.com>
2018-11-20 15:44:46 -0600

    [home] fixes to messaging on home page

--- src/components/home.js
+++ src/components/home.js
@@ -8,19 +8,28 @@ const Home = () => {
     <div>
       <h1 style={{ fontSize: '40px' }}>Why Call Eddie?</h1>
       <p>
-        Remote Diagnostic: A professional will asses your problem BEFORE coming
-        to your house, ensuring you have an accurate estimate for both time and
-        cost, and that the technician will have all the right parts when he
-        shows up at your door.
+        Remote Diagnostic: Using the video chat feature on our app, a
+        professional will asses your repair and let you know if you need to do
+        anything before your technician shows up.
+      </p>
+      <p>
+        Transparency: Because of the remote diagnostic, we can give an accurate
+        estimate of the time and money it will take to fix it BEFORE coming to
+        your house.
+      </p>
+      <p>
+        Efficiency: Knowing what the issue is and how long it will take before
+        dispatching a technician ensures we have the right parts when we show up
+        the first time, not next week on a follow-up visit.
       </p>
       <p>
         Repair Database: Easily search for previous repairs, without going
         through stacks of old receipts.
       </p>
-      <p>
+      {/* <p>
         Scheduling: Take a look at our calendar to check for available times,
         making it easy to know how quickly a technician can come.
-      </p>
+      </p> */}
       <div id="home-boxes">
         <div
           id="home-p1"
