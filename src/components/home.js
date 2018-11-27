63d2fdde2505237aa0b56101d5387e9e16dd65ec
nangelos <nicksangelos@gmail.com>
2018-11-27 12:24:23 -0600

    [App.css] styling

--- src/components/home.js
+++ src/components/home.js
@@ -5,31 +5,48 @@ import architect from '../styles/architect.jpeg';
 
 const Home = () => {
   return (
-    <div>
-      <h1 style={{ fontSize: '40px' }}>Why Call Eddie?</h1>
-      <p>
-        Remote Diagnostic: Using the video chat feature on our app, a
-        professional will asses your repair and let you know if you need to do
-        anything before your technician shows up.
-      </p>
-      <p>
-        Transparency: Because of the remote diagnostic, we can give an accurate
-        estimate of the time and money it will take to fix it BEFORE coming to
-        your house.
-      </p>
-      <p>
-        Efficiency: Knowing what the issue is and how long it will take before
-        dispatching a technician ensures we have the right parts when we show up
-        the first time, not next week on a follow-up visit.
-      </p>
-      <p>
-        Repair Database: Easily search for previous repairs, without going
-        through stacks of old receipts.
-      </p>
-      {/* <p>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        // backgroundColor: 'rgb(237, 225, 229)',
+      }}
+    >
+      <h1 id="home-header">Why Call Eddie?</h1>
+      <div>
+        <div className="reason-box1">
+          <p>
+            <strong>Remote Diagnostic:</strong> Using the video chat feature on
+            our app, a professional will asses your repair and let you know if
+            you need to do anything before your technician shows up.
+          </p>
+        </div>
+        <div className="reason-box2">
+          <p>
+            <strong>Transparency:</strong> Because of the remote diagnostic, we
+            can give an accurate estimate of the time and money it will take to
+            fix it BEFORE coming to your house.
+          </p>
+        </div>
+        <div className="reason-box1">
+          <p>
+            <strong>Efficiency:</strong> Knowing what the issue is and how long
+            it will take before dispatching a technician ensures we have the
+            right parts when we show up the first time, not next week on a
+            follow-up visit.
+          </p>
+        </div>
+        <div className="reason-box2">
+          <p>
+            <strong>Repair Database:</strong> Easily search for previous
+            repairs, without going through stacks of old receipts.
+          </p>
+        </div>
+        {/* <p>
         Scheduling: Take a look at our calendar to check for available times,
         making it easy to know how quickly a technician can come.
       </p> */}
+      </div>
       <div id="home-boxes">
         <div
           id="home-p1"
