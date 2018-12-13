f269b326bb3638b6ddeca94e409923629f98f6a7
nangelos <nicksangelos@gmail.com>
2018-12-13 08:42:38 -0600

    fixed typo

--- src/constants.js
+++ src/constants.js
@@ -14,7 +14,7 @@
 // ];
 
 export const aboutUs = [
-  'Eddie was started with the vision of making it as easy as possible for people to repair their homes. By leveraging emerging technology, we believe that the customer experience can be reimagined in the $400 billion home improvement industry by providing the convenience customers expecte in other industries that have been disrupted by technology.',
+  'Eddie was started with the vision of making it as easy as possible for people to repair their homes. By leveraging emerging technology, we believe that the customer experience can be reimagined in the $400 billion home improvement industry by providing the convenience customers expect in other industries that have been disrupted by technology.',
   "When your furnace goes out, or your refrigerator needs to be repaired, it can be difficult to find a reputable repairman, schedule an appointment, and then miss a day of work while waiting for the technician to come between a four hour block. Once the technician comes to your house, it takes 30 minutes to assess the problem, give you an estimate, and then inform you that he doesn't have the required part in his truck, meaning he will have to come back later in the week, causing you to miss another day of work. We think this process is broken and set out to create a new process that is easy, transparent, and convenient for you.",
 ];
 
