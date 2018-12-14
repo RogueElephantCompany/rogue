e8abd5d835d18ad2e96a3178d5d491a363831a95
nangelos <nicksangelos@gmail.com>
2018-12-14 14:49:45 -0600

    [messages.js] added model

--- server/db/models/messages.js
+++ server/db/models/messages.js
@@ -1,1 +1,21 @@
 const Sequelize = require('sequelize');
+
+const db = require('../db');
+
+const Message = db.define('message', {
+  contactName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  contactPhone: {
+    type: Sequelize.STRING,
+    defaultValue: '',
+  },
+  contactMessage: {
+    type: Sequelize.STRING,
+    defaultValue: '',
+    allowNull: false,
+  },
+});
+
+module.exports = Message;
