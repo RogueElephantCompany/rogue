c7350576317d3fcf0630318a5ab3f931152dc00d
nangelos <nicksangelos@gmail.com>
2018-12-14 15:46:04 -0600

    adding required packages

---
+++ server/index.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const compression = require('compression');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const db = require('./db');
+const sessionStore = new SequelizeStore({ db });
+const PORT = process.env.PORT || 8080;
+const app = express.app();
+module.exports = app;
+
+// if (process.env.NODE_ENV !== 'production') require('../secrets')
+
+const createApp = () => {
+  // logging middleware
+  app.use(morgan('dev'));
+
+  // body parsing middleware
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  // compression middleware
+  app.use(compression());
+  //api routes
+  app.use('/api', require('./api'));
+
+  // static file-serving middleware
+  app.use(express.static(path.join(__dirname, '..', 'public')));
+  // any remaining requests with an extension (.js, .css, etc.) send 404
+  app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error('Not found');
+      err.status = 404;
+      next(err);
+    } else {
+      next();
+    }
+  });
+
+  //error handling middleware
+  app.use((err, req, res, next) => {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error.');
+  });
+};
+
+const startListening = () => {
+  //start listening and create server object representing server
+  const server = app.listen(PORT, () =>
+    console.log(`Mixing it up on port ${PORT}`)
+  );
+};
+
+const syncDB = () => db.sync();
+
+// This evaluates as true when this file is run directly from the command line,
+// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
+// It will evaluate false when this module is required by another module - for example,
+// if we wanted to require our app in a test spec
+if (require.main === module) {
+  sessionStore
+    .sync()
+    .then(syncDb)
+    .then(createApp)
+    .then(startListening);
+} else {
+  createApp();
+}
