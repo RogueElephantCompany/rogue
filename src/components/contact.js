d67e37814ae8e67116eef58477abd0317bdca87d
nangelos <nicksangelos@gmail.com>
2019-01-23 15:10:34 -0600

    [contact.js] adding response after message sent

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,6 +5,7 @@ class Contact extends Component {
   constructor() {
     super();
     this.state = {
+      sent: true,
       contactName: '',
       contactPhone: '',
       contactMessage: '',
@@ -24,50 +25,61 @@ class Contact extends Component {
   render() {
     return (
       <div>
-        <h2 className="page-header">Want to learn more?</h2>
-        <h4>Message us here or email us at info@fixiteddie.com</h4>
-        <form>
-          <div id="contact-form">
-            <div className="line" id="contact-email">
-              <input
-                type="text"
-                className="contact-input"
-                name="contactName"
-                placeholder="YOUR EMAIL ADDRESS*"
-                required={true}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="line" id="contact-phone">
-              <input
-                type="text"
-                className="contact-input"
-                name="contactPhone"
-                placeholder="YOUR PHONE NUMBER*"
-                required={true}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="line" id="message">
-              <textarea
-                name="contactMessage"
-                className="notes-box"
-                placeholder="YOUR MESSAGE*"
-                required={true}
-                onChange={this.handleChange}
-              />
-            </div>
-            <br />
-            <div id="buttonHolder">
-              <input
-                type="submit"
-                value="SUBMIT"
-                id="submit-btn"
-                onClick={this.handleSubmit}
-              />
-            </div>
+        {this.state.sent === false ? (
+          <div>
+            <h2 className="page-header">Want to learn more?</h2>
+            <h4>Message us here or email us at info@fixiteddie.com</h4>
+            <form>
+              <div id="contact-form">
+                <div className="line" id="contact-email">
+                  <input
+                    type="text"
+                    className="contact-input"
+                    name="contactName"
+                    placeholder="YOUR EMAIL ADDRESS*"
+                    required={true}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="line" id="contact-phone">
+                  <input
+                    type="text"
+                    className="contact-input"
+                    name="contactPhone"
+                    placeholder="YOUR PHONE NUMBER*"
+                    required={true}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="line" id="message">
+                  <textarea
+                    name="contactMessage"
+                    className="notes-box"
+                    placeholder="YOUR MESSAGE*"
+                    required={true}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <br />
+                <div id="buttonHolder">
+                  <input
+                    type="submit"
+                    value="SUBMIT"
+                    id="submit-btn"
+                    onClick={this.handleSubmit}
+                  />
+                </div>
+              </div>
+            </form>
           </div>
-        </form>
+        ) : (
+          <div>
+            <h1 style={{ marginTop: '300px' }}>Thank you for contacting us!</h1>
+            <p style={{ marginBottom: '300px' }}>
+              We will respond as soon as we are able...
+            </p>
+          </div>
+        )}
       </div>
     );
   }
